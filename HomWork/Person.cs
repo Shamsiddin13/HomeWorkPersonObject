acf31fbe470bbf9384fb2ed1890e86f189f3f547
Shamsiddin_Dev <shamsiddinumarov0013@gmail.com>
2023-06-15 17:52:51 +0500

    Add project files.

---
+++ HomWork/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomWork
+{
+    internal class Person
+    {
+        public Person(string Name, bool IsParents, Person Jufti)
+        {
+            this.Name = Name;
+            this.IsParents = IsParents;
+            this.Jufti = Jufti;
+        }
+
+        public bool IsParents;
+        
+        public string Name;
+        
+        public Person Jufti;
+
+        static public void Oila(Person person1, Person person2)
+        {
+            person1.IsParents = true;
+            person2.IsParents = true;
+        }
+
+        public void OilaQurdirish(Person person)
+        {
+            Jufti = person;
+            
+        }
+
+        public void Information()   
+        {
+            if (this.IsParents && IsParents) 
+            {
+                Console.WriteLine($"{this.Name} va {Jufti.Name} oila qurishdi");
+            }
+        }
+
+    }
+}
---
+++ HomWork/Program.cs
@@ -0,0 +1,10 @@
+﻿using HomWork;
+
+Person person1 = new Person("Alisher", false, null);
+
+Person person2 = new Person("Dilnoza",false,null);
+
+Person.Oila(person1, person2);
+person1.OilaQurdirish(person2);
+
+person1.Information();
